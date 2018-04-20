daa4aa6746e46ee77ce7a19b902334260139fa90
SidSoft <sidsoftlab@gmail.com>
2018-04-20 14:43:48 +0300

    fixed pagination links

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -15,6 +15,7 @@ class ArticleController extends Controller
     public function index()
     {
         $articles = Article::orderBy('created_at', 'desc')->paginate(6);
+        $articles->setPath('/');
 
         return ArticleResource::collection($articles);
     }
